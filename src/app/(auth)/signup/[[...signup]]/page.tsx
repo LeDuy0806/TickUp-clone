fcf1fc2db9440c692409e240ac4602aca5d23154
Le Duy <duylangtu931@gmail.com>
2023-12-28 22:48:43 +0700

    feat: add clerk authentication

---
+++ src/app/(auth)/signup/[[...signup]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs';
+import React from 'react';
+
+// type Props = {}
+
+const LoginPage: React.FC = (props) => {
+    return <SignUp />;
+};
+
+export default LoginPage;
---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { PropsWithChildren } from 'react';
+
+const DashboardLayout: React.FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <main className="w-full h-screen flex items-center justify-center">
+            {children}
+        </main>
+    );
+};
+
+export default DashboardLayout;
