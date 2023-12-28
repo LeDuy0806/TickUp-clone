a952d84eb1b0e3fb191a4fd968c3425532c3a76a
Le Duy <duylangtu931@gmail.com>
2023-12-29 00:02:55 +0700

    feat: add tickup landing page + setup layout dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,27 @@
-import { useUser } from '@clerk/nextjs';
+'use client';
+
+import { OrganizationList, useUser } from '@clerk/nextjs';
 import React from 'react';
 
 const DashboardPage: React.FC = (props) => {
     const { user } = useUser();
     return (
-        <div>
+        <div className="container w-full flex flex-col items-center justify-center">
             <h1 className="text-3xl">Dashboard Page</h1>
-            <p>
+            <p className="text-base">
                 {JSON.stringify({
                     email: user?.emailAddresses,
                     name: user?.fullName,
                 })}
             </p>
-            {user?.imageUrl && <img src={user?.imageUrl} alt="User" />}
+            {user?.imageUrl && (
+                <img width={50} src={user?.imageUrl} alt="User" />
+            )}
+            <OrganizationList
+                hidePersonal
+                afterCreateOrganizationUrl="/workspace/:id"
+                afterSelectOrganizationUrl="/workspace/:id"
+            />
         </div>
     );
 };
